989427147bdd9daf5ef92159ae33dec191b6bbff
NeverMind109 <vladgladkih123@gmail.com>
2022-05-16 05:03:22 +0300

    Created: Finished Page + Adaptive + Modal

--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -4,7 +4,7 @@ Swiper.use([Navigation, Scrollbar]);
 const trainers = document.querySelector(".trainers__content");
 const trainersSlider = new Swiper(trainers, {
   slidesPerView: "auto",
-  spaceBetween: 40,
+  spaceBetween: 20,
   scrollbar: {
     el: ".trainers__scroll",
     draggable: true,
@@ -12,5 +12,20 @@ const trainersSlider = new Swiper(trainers, {
   navigation: {
     nextEl: ".trainers__slider-btn--next",
     prevEl: ".trainers__slider-btn--prev",
+  },
+  breakpoints: {
+    // when window width is >= 576px
+    576: {
+      slidesPerView: "auto",
+      spaceBetween: 40,
+      scrollbar: {
+        el: ".trainers__scroll",
+        draggable: true,
+      },
+      navigation: {
+        nextEl: ".trainers__slider-btn--next",
+        prevEl: ".trainers__slider-btn--prev",
+      },
+    },
   },
 });
