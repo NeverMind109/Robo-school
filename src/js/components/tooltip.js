b5249f9017b8ef1f834f67566caa59b65b448854
NeverMind109 <vladgladkih123@gmail.com>
2022-05-16 18:06:37 +0300

    Added: popper fix

--- src/js/components/tooltip.js
+++ src/js/components/tooltip.js
@@ -4,18 +4,12 @@ const el = document.querySelector(".tooltip__btn");
 const tooltip = document.querySelector(".tooltip__txt");
 
 createPopper(el, tooltip, {
-  placement: "top-start",
+  placement: "top",
   modifiers: [
     {
       name: "offset",
       options: {
         offset: [0, 10],
-      },
-    },
-    {
-      name: "preventOverflow",
-      options: {
-        mainAxis: false, // true by default
       },
     },
   ],
